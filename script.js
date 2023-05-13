af9fe218212ffea67163b07e3be0bc16d5b3825d
Pedro Mendonça <113257301+luffyamao@users.noreply.github.com>
2023-05-13 17:32:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cat = document.querySelector('.cat');
+const tree = document.querySelector('.tree');
 
 const jump = () => {
     cat.classList.add('jump');
@@ -9,5 +10,25 @@ const jump = () => {
 
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const treePosition = tree.offsetLeft;
+    const catPosition = +window.getComputedStyle(cat).bottom.replace('px', '');
+
+    if (treePosition <= 120 && treePosition  > 0 && catPosition < 130) {
+
+        tree.style.animation = 'none';
+        tree.style.left = `${treePosition}px`;
+
+        cat.style.animation = 'none';
+        cat.style.bottom = `${catPosition}px`;
+
+        cat.src = 'https://github.com/luffyamao/teste/blob/main/Anime-Cat-Transparent-Background.png?raw=true';
+        cat.style.width = '200px';
+
+        clearInterval(loop);
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
