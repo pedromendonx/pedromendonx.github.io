934d1d0e853a147f25b2a1fec1b2a6e7215e7b5d
Pedro Mendonça <113257301+luffyamao@users.noreply.github.com>
2023-11-01 10:11:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,26 @@
-const cat = document.querySelector('.cat');
-const tree = document.querySelector('.tree');
-const button = document.querySelector('.buttonstyle');
-const nuvem = document.querySelector('.nuvens');
-const fimdejogo = document.querySelector('.fimdejogo');
-const score = document.querySelector('.score');
+let result = document.getElementById("inputext");
 
-let count = 0;
+let calcular=(number)=> {
+    result.value+=number;
 
-const jump = () => {
-    cat.classList.add('jump');
-
-    setTimeout(() => {
-
-        cat.classList.remove('jump');
-
-    }, 500);
 }
 
-
-
-
-const loop = setInterval(() => {
-
-    const treePosition = tree.offsetLeft;
-    const catPosition = +window.getComputedStyle(cat).bottom.replace('px', '');
-    //const nuvemPosition = window.getComputedStyle(nuvem)
-
-
-    if (treePosition <= 120 && treePosition  > 0 && catPosition < 130) {
-
-        tree.style.animation = 'none';
-        tree.style.left = `${treePosition}px`;
-
-        cat.style.animation = 'none';
-        cat.style.bottom = `${catPosition}px`;
-
-        cat.src = 'https://github.com/luffyamao/teste/blob/main/Anime-Cat-Transparent-Background.png?raw=true';
-        cat.style.width = '200px';
-
-        //nuvem.style.animation = 'none';
-
-        fimdejogo.style.visibility = 'visible';
-
-        clearInterval(loop);
+let Result=()=>{
+    var result = document.getElementById('result').innerHTML;
+    if(result)
+    {
+        document.getElementById('result').innerHTML = eval(result);
     }
-    count++;
-    score.innerHTML = `SCORE: ${count}`;
-}, 10);
+    else
+    {
+        document.getElementById('result').innerHTML = "Nada..."
+}
+}
 
-document.addEventListener('keydown', (e) => {
-    if ((e.code === 'ArrowUp') | (e.code === 'Space')){
-        jump();
-    }
+function allclear() {
+    result.value = " ";
+}
 
-
-});
+function delet() {
+ result.value = result.value.slice(0,-1);
+}
