1083fb2eed28c60485dc0198f3a21096ccf64ae1
luffyamao <kfp3drod15@gmail.com>
2024-01-10 11:22:36 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-var NumBin = document.getElementById("inputBinario");
-
-function TesteBinario(e) {
-    var tecla=(window.event)?e.keyCode:e.which;
-    if((tecla==48 || tecla==49)) return true;
-     else{
-    return false;
-     }
-}
-
 function Calcular() {
-    var NumDecimal = parseInt(NumBin, 2);
-    console.log(NumDecimal);
+    const NumBin = document.getElementById("inputBinario").value;
+    const NumDecimal = parseInt(NumBin, 2);
+    
+
+    document.getElementById("resultado").innerText = `Número em decimal ${NumDecimal}`;
+
+
 
 }
